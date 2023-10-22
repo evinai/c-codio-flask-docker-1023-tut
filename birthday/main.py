54d3786887ecdfdf01f5ed0b56f19e34c8cf1e65
Togay Tunca <tuncatogay@gmail.com>
2023-10-22 21:10:26 +0000

    updated main birthday check

--- birthday/main.py
+++ birthday/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -11,10 +12,26 @@ def birthday():
 def about():
     return render_template('about.html')
 
-
-@app.route('/check_birthday')
+@app.route('/check_birthday', methods=['POST'])
 def check_birthday():
-    pass
+    user_month = request.form['month']
+    user_day = request.form['day']
+    dt_obj = datetime.now()
+    today_month = dt_obj.strftime('%B')
+    today_day = dt_obj.strftime('%d')
+
+    check_month = today_month == user_month
+    check_day = today_day == user_day
+
+    if check_month and check_day:
+        msg = 'Happy birthday!'
+        txt = 'Today is your birthday. Spend it doing something you enjoy with those you enjoy being around.'
+    else:
+        msg = 'It is not your birthday'
+        txt = 'Sadly, today is not your birthday. Try again later.'
+        
+    return render_template('check.html', value=(msg, txt))
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
